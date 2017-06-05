4ef00d30e41a94815defd71d223f959510a12905
WSiaB <johnl@kirkersoft.com>
2017-06-04 20:09:50 -0700

    removed IP from display (replaced with LCD)

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ $localIP = exec ($command);
 ?>
 <html>
 <body>
-<iframe src="https://app.smartsheet.com/b/publish?EQBCT=622a2c8c8881490b8007c1d84bc14653" width=100% height=97%></iframe>
-NOTE: Changes may take up to 5 minutes to appear. IP:<?php echo $localIP; ?>.
+<iframe src="https://app.smartsheet.com/b/publish?EQBCT=622a2c8c8881490b8007c1d84bc14653" width=100% height=100%></iframe>
 </body>
 </html>
